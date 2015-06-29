2df6dcd3430a5842164acc9120bc8a171b8e6abe
alexf <alexandro.sousa@gmail.com>
2015-06-29 01:19:11 -0300

    Configuração inicial do Spring Data Jpa

---
+++ src/main/java/com/alexandrofs/gfp/config/Config.java
@@ -0,0 +1,53 @@
+package com.alexandrofs.gfp.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages="com.alexandrofs.gfp.persist.repository")
+@EnableTransactionManagement
+@ComponentScan(basePackages="com.alexandrofs.gfp")
+public class Config {
+
+    @Bean
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL).ignoreFailedDrops(true)
+                .addScript("schema.sql")
+                .build();
+        return db;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+        entityManagerFactoryBean.setPackagesToScan("com.alexandrofs.gfp.model");
+        entityManagerFactoryBean.setJpaProperties(hibernateProperties());
+        return entityManagerFactoryBean;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+        properties.put("hibernate.show_sql", "false");
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.jdbc.batch_size", "100");
+        return properties;
+    }
+
+
+}
